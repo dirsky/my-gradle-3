c8faa15b2652c16f19caff183d4a8a277cbd9a31
guozhong.xu <guozhong.xu@qq.com>
2020-12-31 16:15:58 +0800

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import cn.hutool.core.convert.Convert;
+
+/**
+ * @author: Guozhong Xu
+ * @description:
+ * @date: Create in 15:41 2020-12-31
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Convert.toStr(110));
+        System.out.println("hi");
+    }
+}
